9064ca9a44f089c1eeac4a3cbc992cb5043b1e18
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-02-27 18:03:10 -0300

    Update Aula1.js

--- JS_LO/01JS_Básico/Aula1/Aula1.js
+++ JS_LO/01JS_Básico/Aula1/Aula1.js
@@ -1,1 +1,8 @@
-console.log('Olá, Mundo!')
+console.log('Olá, Mundo!'); //String
+console.log("Olá, Mundo!");
+
+console.log(`Otávio Wolff`);
+
+console.log('Otávio "Wolff"');
+
+console.log(56, 1.13, 'Olá');
